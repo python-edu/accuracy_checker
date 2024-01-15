5c12602a3f3ce6da8819f68065596e4ad3522dd5
Piotr Kramarczy <pieciak1@poczta.onet.pl>
2024-01-15 12:03:12 +0100

    On branch master Changes to be committed:       modified:   acc/main.py
          modified:   acc/src/args.py
                      - zmiany zawiązane ze aktualizacją modułu `verbosepk`,
                        z pakietu `simpleutils`
    
          new file:   acc/src/funkcje.py
                      - dwie funkcje do konwersji np.array i pd.DataFRame na
                        listę list (list[list]), na potrzeby `verbosepk`
    
          modified:   requirements.txt

--- acc/main.py
+++ acc/main.py
@@ -3,13 +3,14 @@ import numpy as np
 import pandas as pd
 
 # my modules import
-from simpleutils.src import verbosepk as vb
+from simpleutils.src import verbosepk
 from acc_pack.src import metryki
 
 # local imports
 from acc.src import args as parser  # parser argumentów w osobnym pliku
 from acc.src import read_data  # funkcje do odczytu różnych danych
 from acc.src.raport_base import SimpleRaport
+from acc.src import funkcje as fn
 
 # -----------------------------------------------------------------------------
 
@@ -85,81 +86,91 @@ def main():
     # =========================================================================
     args = parser.parsuj_argumenty()
     args = parser.validuj_args(args)
-    vb.verbose(pars=True, **vars(args))
+    vb = verbosepk.Verbose()
+    vb(verbose=args.verbose, args=vars(args))
 
-    # =========================================================================
-    # 2. Odczyt danych wejściowych
-    # =========================================================================
-    all_data = read_data.read_data(args)
-    data, cros, cros_full, binary_cros, binary_cros1 = all_data
-    vb.verbose(verbose=args.verbose, data=data, cros=cros, cros_full=cros_full,
-               binary_cros=binary_cros, binary_cros1=binary_cros1)
+    # ta opcja wyświetla informacje o statystykach blokując wszystko inne!!!
+    if args.info:
+        print(parser.info)
 
-    # =========================================================================
-    # 3. Tradycyjne, klasyczne wskaźniki dokładności
-    # =========================================================================
-    classic_acc = acc_from_cros(cros, args)
-    vb.verbose(verbose=True, classic_acc=classic_acc)
-
-    # =========================================================================
-    # 4. Nowe wskaźniki dokładności
-    # =========================================================================
-    modern1, modern2 = acc_from_bin_cros(binary_cros, args)
-    vb.verbose(verbose=True, modern1=modern1, modern2=modern2)
-
-    # =========================================================================
-    # 4.1. Liczy średnie wartości wskaźników 'modern1' i 'modern2'
-    # =========================================================================
-
-    m1 = np.round(modern1.mean(), 4)
-    m2 = np.round(modern2.mean(), 4)
-
-    modern_mean = pd.DataFrame(pd.concat([m1, m2]))
-    modern_mean.columns = ['Value']
-
-    vb.verbose(verbose=True, modern_mean=modern_mean)
-
-    # =========================================================================
-    # 5. Zapisywanie danych
-    # =========================================================================
-    names = ["cros", "binary_cros", "classic_acc", "modern1", "modern2"]
-
-    if args.save:
-        pths = [args.out_dir / f'{n}.csv' for n in names]
-        pths = [str(p) for p in pths]
-        args.out_dir.mkdir(exist_ok=True)
-
-        vb.verbose(verbose=True, zapis="""\tZapisywanie plików `csv`:\n""")
-        zapisano = []
-
-        for i, nazwa in enumerate(names):
-            if nazwa == 'cros':
-                nazwa = 'cros_full'
-
-            if nazwa == "binary_cros":
-                nazwa = "binary_cros1"
-
-            data = locals()[nazwa]
-            ad = pths[i]
-            data.to_csv(ad, sep=args.sep)
-            zapisano.append(ad)
-        vb.verbose(verbose=True, zapisano=zapisano)
-
-    # tworzy raport html
-    if args.raport:
-        vb.verbose(verbose=args.verbose,
-                   raport='''\n\tTworzenie i zapis raportu:\n''')
-
-        # dane = [locals()[nazwa] for nazwa in names1]
-        dane = [cros_full, binary_cros1, classic_acc, modern1, modern2]
-        raport = AccRaport(data=dane, opis=names)
-        raport.saveRaport(raportAdres=args.raport)
-
-        vb.verbose(verbose=True, raport_zapisany=args.raport)
     else:
-        msg1 = '\t Aby zapisać wyniki do plików csv użyj flagi `-s`.'
-        msg2 = '\t Aby wygenerować raport html użyj flagi `-rap`.'
-        vb.verbose(verbose=True, Save=msg1, Raport=msg2)
+        # =====================================================================
+        # 2. Odczyt danych wejściowych
+        # =====================================================================
+        all_data = read_data.read_data(args)
+        data, cros, cros_full, binary_cros, binary_cros1 = all_data
+        vb(verbose=args.verbose,
+           data=fn.df2list(data.head()), cros=fn.df2list(cros),
+           cros_full=fn.df2list(cros_full),
+           binary_cros=fn.df2list(binary_cros),
+           binary_cros1=fn.df2list(binary_cros1))
+
+        # =====================================================================
+        # 3. Tradycyjne, klasyczne wskaźniki dokładności
+        # =====================================================================
+        classic_acc = acc_from_cros(cros, args)
+        vb(verbose=True, classic_acc=fn.df2list(classic_acc))
+
+        # =====================================================================
+        # 4. Nowe wskaźniki dokładności
+        # =====================================================================
+        modern1, modern2 = acc_from_bin_cros(binary_cros, args)
+        vb(verbose=True, modern1=fn.df2list(modern1),
+           modern2=fn.df2list(modern2))
+
+        # =====================================================================
+        # 4.1. Liczy średnie wartości wskaźników 'modern1' i 'modern2'
+        # =====================================================================
+
+        m1 = np.round(modern1.mean(), 4)
+        m2 = np.round(modern2.mean(), 4)
+
+        modern_mean = pd.DataFrame(pd.concat([m1, m2]))
+        modern_mean.columns = ['Value']
+
+        vb(verbose=True, modern_mean=fn.df2list(modern_mean))
+
+        # =====================================================================
+        # 5. Zapisywanie danych
+        # =====================================================================
+        names = ["cros", "binary_cros", "classic_acc", "modern1", "modern2"]
+
+        if args.save:
+            pths = [args.out_dir / f'{n}.csv' for n in names]
+            pths = [str(p) for p in pths]
+            args.out_dir.mkdir(exist_ok=True)
+
+            vb(verbose=True, zapis="""\tZapisywanie plików `csv`:\n""")
+            zapisano = []
+
+            for i, nazwa in enumerate(names):
+                if nazwa == 'cros':
+                    nazwa = 'cros_full'
+
+                if nazwa == "binary_cros":
+                    nazwa = "binary_cros1"
+
+                data = locals()[nazwa]
+                ad = pths[i]
+                data.to_csv(ad, sep=args.sep)
+                zapisano.append(ad)
+            vb(verbose=True, zapisano=zapisano)
+
+        # tworzy raport html
+        if args.raport:
+            vb(verbose=args.verbose,
+               raport='''\n\tTworzenie i zapis raportu:\n''')
+
+            # dane = [locals()[nazwa] for nazwa in names1]
+            dane = [cros_full, binary_cros1, classic_acc, modern1, modern2]
+            raport = AccRaport(data=dane, opis=names)
+            raport.saveRaport(raportAdres=args.raport)
+
+            vb(verbose=True, raport_zapisany=args.raport)
+        else:
+            msg1 = '\t Aby zapisać wyniki do plików csv użyj flagi `-s`.'
+            msg2 = '\t Aby wygenerować raport html użyj flagi `-rap`.'
+            vb(verbose=True, Save=msg1, Raport=msg2)
 # ---
 
 
--- acc/src/args.py
+++ acc/src/args.py
@@ -18,8 +18,6 @@ from simpleutils.src import argspk as apk
 
 # --
 
-# szerokość tekstu dla wyświtlania pomocy
-wt = 80
 
 description = """
 Skrypt sprawdza jakość klasyfikacji:
@@ -75,81 +73,84 @@ apk.mapa = mapa_args()
 def parsuj_argumenty():
     '''
     '''
-    parser = argparse.ArgumentParser(
-            formatter_class=argparse.RawTextHelpFormatter,
+
+    parser = apk.MyParserWithDefaults(
+            formatter_class=apk.MyHelpFormatter,
             description=description,
             fromfile_prefix_chars='@',
             )
 
     parser.convert_arg_line_to_args = apk.convert_arg_line
 
-    txt = '''Adres pliku 'csv' z danymi. Dany mogą być:
-    1. Surowe dane - przynajmniej dwie kolumny:
-    ----------------------------
-    |    true   | predicted |
-    | --------- | --------- |
-    |        1  |     2     |
-    |        5  |     3     |
-    ----------------------------
-    :
-    lub
-    :
-    --------------------------------------
-    |  etykieta |    true   | predicted |
-    | --------- | --------- | --------- |
-    |   trawa   |        1  |     2     |
-    |    woda   |        5  |     3     |
-    --------------------------------------
-    :
-    .    gdzie:
-    .       - 'predicted' - wynik klasyfikacji, np. 5
-    .       - 'true'      - prawdziwa etykieta klasy np. 7.
+    txt = '''\
+    Adres pliku 'csv' z danymi. Dany mogą być:
+      1. Surowe dane - przynajmniej dwie kolumny:
+         -------------------------
+         |    true   | predicted |
+         | --------- | --------- |
+         |        1  |     2     |
+         |        5  |     3     |
+         -------------------------
 
-    2. Cross matrix - gotowa tabela z opisami kolumn/wierszy i sumami.
-    3. binTF - tabela TP, TN, FP, FN.'''
-    parser.add_argument('input', type=str, help=txt)
+         lub
+
+         ------------------------------------
+         | etykieta |    true   | predicted |
+         | -------- | --------- | --------- |
+         |  trawa   |        1  |     2     |
+         |   woda   |        5  |     3     |
+         ------------------------------------
+
+         gdzie:
+           - 'predicted' - wynik klasyfikacji, np. 5
+           - 'true'      - prawdziwa etykieta klasy np. 7.
+
+      2. Cross matrix - gotowa tabela z opisami kolumn/wierszy i sumami.
+      3. binTF - tabela TP, TN, FP, FN.'''
+    parser.add_argument('input', type=str, help=textwrap.dedent(txt))
 
     # ---
 
-    txt = '''Mówi czym są dane wejściowe. Możliwości:
-        - 'data' - 2 lub 3 kolumny w pliku csv
-        - 'cros_raw' - cros matrix bez opisów wierszy i kolumn - same liczby
-        - 'cros' - cros matrix z opisami wierszy i kolumn, bez sum
-        - 'cros_full' - cros matrix z opisami kolumn i wierszy i z sumami
-          wierszy i kolumn
-        - 'bin' binTF.
-
-        Domyślnie 'data'.'''
+    txt = '''\
+        Mówi czym są dane wejściowe. Możliwości:
+          - 'data' - 2 lub 3 kolumny w pliku csv
+          - 'cros_raw' - cros matrix bez opisów wierszy i kolumn - same liczby
+          - 'cros' - cros matrix z opisami wierszy i kolumn, bez sum
+          - 'cros_full' - cros matrix z opisami kolumn i wierszy i z sumami
+            wierszy i kolumn
+          - 'bin' binTF.'''
     parser.add_argument('-d', '--data_type', type=str,
-                        help=txt, default='data')
+                        help=textwrap.dedent(txt), default='data')
     # -------------------------------------------------------------------------
 
-    txt = '''Dokładność. Domyślnie 4 miejsca po przecinku.'''
-    parser.add_argument('-p', '--precision', type=int,
-                        help=textwrap.fill(txt, width=wt), default=4)
+    txt = '''Wyświetla informacje o obliczanych statystykach.'''
+    parser.add_argument('-i', '--info', help=txt, action='store_true')
 
-    txt = 'Wskazuje, że układ cross matrix jest odwrócony tzn. w kolumnach' \
-        'są dane referencyjne (true) a w wierszach są wyniki klasyfikacji' \
-        'obrazu (predict).'
-    parser.add_argument('-r', '--revers', action='store_true',
-                        help=textwrap.fill(txt, width=wt))
+    # -------------------------------------------------------------------------
+
+    txt = '''Dokładność - liczba miejsca po przecinku.'''
+    parser.add_argument('-p', '--precision', type=int, help=txt, default=4)
+
+    txt = 'Gdy cross matrix to dane wejściowe, to flaga wskazuje, że układ' \
+        ' cross matrix jest odwrócony tzn. w kolumnach' \
+        ' są dane referencyjne (true) a w wierszach są wyniki klasyfikacji' \
+        ' obrazu (predict).'
+    parser.add_argument('-r', '--revers', action='store_true', help=txt)
 
     txt = 'Dotyczy danych typu `crossmatrix`: zaznacz jeśli dane zawierają ' \
-        'podsumowaie wierszy i kolumn. Domyślnie nie!'
-    parser.add_argument('-ss', '--sums', action='store_true',
-                        help=textwrap.fill(txt, width=wt))
+        ' podsumowaie wierszy i kolumn.'
+    parser.add_argument('-ss', '--sums', action='store_true', help=txt)
 
     # -------------------------------------------------------------------------
 
-    txt = "Określa separator kolumn pliku csv. Domyślnie to średnik ';'."
-    parser.add_argument('-sp', '--sep', type=str,
-                        help=textwrap.fill(txt, width=wt), default=';')
+    txt = "Określa separator kolumn pliku csv."
+    parser.add_argument('-sp', '--sep', type=str, help=txt, default=';')
 
-    txt = "Str, nazwa katalogu do zapisu danych. Domyślnie `results`." \
-          "Katalog tworzony jest w katalogu roboczym, czyli nadrzędnym" \
-          "do katalogu z danymi wejściowymi."
+    txt = "Str, nazwa katalogu do zapisu danych." \
+          " Katalog tworzony jest w katalogu roboczym, czyli nadrzędnym" \
+          " do katalogu z danymi wejściowymi."
     parser.add_argument('-o', '--out_dir', type=str,
-                        help=textwrap.fill(txt, width=wt), default='results')
+                        help=txt, default='results')
 
     txt = '''Domyślnie skrypt wyświetla wyniki na ekranie. Ta opcja powoduje,
     zapisanie wyników do osobnych plików csv:
@@ -168,21 +169,18 @@ def parsuj_argumenty():
     # -------------------------------------------------------------------------
 
     txt = '''Adres pliku 'csv' z danymi referencyjnymi - 2 kolumny:
-        - 'label;name'.
+    - 'label;name'.
         '''
     parser.add_argument('-rf', '--ref', type=str, help=txt, default=None)
 
     txt = "Wskazuje, że wynikiem skryptyu ma być tylko raport.html. " \
-        " Domyślnie oprócz raportu generuje również pliki csv."
-    parser.add_argument('-f', '--full_save',
-                        help=textwrap.fill(txt, width=wt),
-                        action='store_true')
+        "Domyślnie oprócz raportu generuje również pliki csv."
+    parser.add_argument('-f', '--full_save', help=txt, action='store_true')
 
     # -------------------------------------------------------------------------
 
     txt = "Wyświetla bardziej szczegółowe informacje."
-    parser.add_argument('-v', '--verbose', help=textwrap.fill(txt, width=wt),
-                        action='store_true')
+    parser.add_argument('-v', '--verbose', help=txt, action='store_true')
 
     args = parser.parse_args()
 
@@ -194,23 +192,24 @@ def validuj_args(args):
     '''Funkcja przetwarza argumenty wejściowe skryptu'''
     # nazwy katalogów i plików wyjściowych
 
-    if args.input:
-        if Path(args.input).expanduser().is_absolute():
-            args.input = Path(args.input).expanduser()
-        else:
-            args.input = Path(args.input).resolve()
+    if not args.info:
+        if args.input:
+            if Path(args.input).expanduser().is_absolute():
+                args.input = Path(args.input).expanduser()
+            else:
+                args.input = Path(args.input).resolve()
 
-        args.work_dir = args.input.parent
+            args.work_dir = args.input.parent
 
-    if args.raport or args.save:
-        args.out_dir = args.work_dir / args.out_dir
+        if args.raport or args.save:
+            args.out_dir = args.work_dir / args.out_dir
 
-        name = Path(args.input).name.split('.')[0]
-        name = f'{name}_raport.html'
-        args.raport = args.out_dir / name
+            name = Path(args.input).name.split('.')[0]
+            name = f'{name}_raport.html'
+            args.raport = args.out_dir / name
 
-    if args.ref is not None:
-        args.ref = Path(args.ref).resolve()
+        if args.ref is not None:
+            args.ref = Path(args.ref).resolve()
 
     return args
 # --
